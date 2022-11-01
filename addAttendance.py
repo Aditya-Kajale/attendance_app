2f014cfdae31b314b80f29ac887ddfa335559f40
Aditya Kajale <66864217+Aditya-Kajale@users.noreply.github.com>
2022-11-01 22:44:13 +0530

    btech subjects done

--- addAttendance.py
+++ addAttendance.py
@@ -15,22 +15,116 @@ fy = at_fy.cursor()
 
 def addAttendance(data,present): 
     print(data,present)
+
     if data[3]=='Theory':
+        # BTECH Year --------------------------------------
         if data[0]=="BTECH":
             if data[4] == "Software Engineering":
-                aditya = data[2].replace('-','_')
-                print(aditya)
+                date = data[2].replace('-','_')
                 try:
-                    sql ='ALTER TABLE se ADD {} int DEFAULT 0'.format(aditya)
+                    sql ='ALTER TABLE se ADD {} int DEFAULT 0'.format(date)
                     btech.execute(sql)
                     at_btech.commit()
-                    sql = "UPDATE se SET {}=1 WHERE roll={} ".format(aditya,i)
+                    sql = "UPDATE se SET {}=1 WHERE roll={} ".format(date,i)
                     btech.execute(sql)
                     at_btech.commit()
                 except:
                     print('already exist')
                     for i in present:
-                        print(i,aditya)
-                        sql = "UPDATE se SET {}=1 WHERE roll={} ".format(aditya,i)
+                        print(i,date)
+                        sql = "UPDATE se SET {}=1 WHERE roll={} ".format(date,i)
                         btech.execute(sql)
                         at_btech.commit()
+
+            elif data[4] == "Big Data Analytics":
+                date = data[2].replace('-','_')
+                print(data[4])
+                try:
+                    sql ='ALTER TABLE bda ADD {} int DEFAULT 0'.format(date)
+                    btech.execute(sql)
+                    at_btech.commit()
+                    sql = "UPDATE bda SET {}=1 WHERE roll={} ".format(date,i)
+                    btech.execute(sql)
+                    at_btech.commit()
+                except:
+                    print('already exist')
+                    for i in present:
+                        print(i,date)
+                        sql = "UPDATE bda SET {}=1 WHERE roll={}".format(date,i)
+                        btech.execute(sql)
+                        at_btech.commit()
+            
+            elif data[4] == "Cloud Computing":
+                date = data[2].replace('-','_')
+                print(data[4])
+                try:
+                    sql ='ALTER TABLE cc ADD {} int DEFAULT 0'.format(date)
+                    btech.execute(sql)
+                    at_btech.commit()
+                    sql = "UPDATE cc SET {}=1 WHERE roll={} ".format(date,i)
+                    btech.execute(sql)
+                    at_btech.commit()
+                except:
+                    print('already exist')
+                    for i in present:
+                        print(i,date)
+                        sql = "UPDATE cc SET {}=1 WHERE roll={}".format(date,i)
+                        btech.execute(sql)
+                        at_btech.commit()
+
+            elif data[4] == "Blockchain Technology":
+                date = data[2].replace('-','_')
+                print(data[4])
+                try:
+                    sql ='ALTER TABLE bt ADD {} int DEFAULT 0'.format(date)
+                    btech.execute(sql)
+                    at_btech.commit()
+                    sql = "UPDATE bt SET {}=1 WHERE roll={} ".format(date,i)
+                    btech.execute(sql)
+                    at_btech.commit()
+                except:
+                    print('already exist')
+                    for i in present:
+                        print(i,date)
+                        sql = "UPDATE bt SET {}=1 WHERE roll={}".format(date,i)
+                        btech.execute(sql)
+                        at_btech.commit()
+
+            elif data[4] == "Full Stack Development":
+                date = data[2].replace('-','_')
+                print(data[4])
+                try:
+                    sql ='ALTER TABLE fsd ADD {} int DEFAULT 0'.format(date)
+                    btech.execute(sql)
+                    at_btech.commit()
+                    sql = "UPDATE fsd SET {}=1 WHERE roll={} ".format(date,i)
+                    btech.execute(sql)
+                    at_btech.commit()
+                except:
+                    print('already exist')
+                    for i in present:
+                        print(i,date)
+                        sql = "UPDATE fsd SET {}=1 WHERE roll={}".format(date,i)
+                        btech.execute(sql)
+                        at_btech.commit()
+
+            elif data[4] == "System Administration":
+                date = data[2].replace('-','_')
+                print(data[4])
+                try:
+                    sql ='ALTER TABLE sa ADD {} int DEFAULT 0'.format(date)
+                    btech.execute(sql)
+                    at_btech.commit()
+                    sql = "UPDATE sa SET {}=1 WHERE roll={} ".format(date,i)
+                    btech.execute(sql)
+                    at_btech.commit()
+                except:
+                    print('already exist')
+                    for i in present:
+                        print(i,date)
+                        sql = "UPDATE sa SET {}=1 WHERE roll={}".format(date,i)
+                        btech.execute(sql)
+                        at_btech.commit()
+
+
+
--- attendanceDBS.py
+++ attendanceDBS.py
@@ -27,7 +27,9 @@ def subjectAttendance (year,division,subject,sdate,edate):
 
     # print(year,type(division),subject,sdate,edate,total)
     
+    # For BTECH -----------------------------------------------
     if year=='BTECH':
+        # For SE -----------------------------------------------
         if subject=="Software Engineering":
             sql = 'SELECT roll,division FROM se WHERE division = "{}"'.format(division)
             btech.execute(sql)
@@ -57,6 +59,160 @@ def subjectAttendance (year,division,subject,sdate,edate):
                     total['roll'][j].append(total[total['dates'][i]][j][0])
 
             total['pre'] = [year,division,subject]
+        
+        # For BDA -----------------------------------------------
+        elif subject=="Big Data Analytics":
+            sql = 'SELECT roll,division FROM bda WHERE division = "{}"'.format(division)
+            btech.execute(sql)
+            data = btech.fetchall()
+
+            row = []
+            for i in data:
+                row.append(list(i))
+            total['roll'] = row
+
+            new_dates = []
+            for i in dates:
+                try:    
+                    sql = 'SELECT {} FROM bda WHERE division = "{}"'.format(i,division)
+                    btech.execute(sql)
+                    data = btech.fetchall()
+                    total[i] = data
+                    new_dates.append(i)
+                except:
+                    print('except')
+            # print(new_dates)
+
+            total['dates'] = new_dates
+
+            for i in range(len(total['dates'])):
+                for j in range(len(total['roll'])):
+                    total['roll'][j].append(total[total['dates'][i]][j][0])
+
+            total['pre'] = [year,division,subject]
+
+
+        # For CC -----------------------------------------------
+        elif subject=="Cloud Computing":
+            sql = 'SELECT roll,division FROM cc WHERE division = "{}"'.format(division)
+            btech.execute(sql)
+            data = btech.fetchall()
+
+            row = []
+            for i in data:
+                row.append(list(i))
+            total['roll'] = row
+
+            new_dates = []
+            for i in dates:
+                try:    
+                    sql = 'SELECT {} FROM cc WHERE division = "{}"'.format(i,division)
+                    btech.execute(sql)
+                    data = btech.fetchall()
+                    total[i] = data
+                    new_dates.append(i)
+                except:
+                    print('except')
+
+            total['dates'] = new_dates
+
+            for i in range(len(total['dates'])):
+                for j in range(len(total['roll'])):
+                    total['roll'][j].append(total[total['dates'][i]][j][0])
+
+            total['pre'] = [year,division,subject]
+
+        # For BT -----------------------------------------------
+        elif subject=="Blockchain Technology":
+            sql = 'SELECT roll,division FROM bt WHERE division = "{}"'.format(division)
+            btech.execute(sql)
+            data = btech.fetchall()
+
+            row = []
+            for i in data:
+                row.append(list(i))
+            total['roll'] = row
+
+            new_dates = []
+            for i in dates:
+                try:    
+                    sql = 'SELECT {} FROM bt WHERE division = "{}"'.format(i,division)
+                    btech.execute(sql)
+                    data = btech.fetchall()
+                    total[i] = data
+                    new_dates.append(i)
+                except:
+                    print('except')
+            # print(new_dates)
+
+            total['dates'] = new_dates
+
+            for i in range(len(total['dates'])):
+                for j in range(len(total['roll'])):
+                    total['roll'][j].append(total[total['dates'][i]][j][0])
+
+            total['pre'] = [year,division,subject]
+        
+        # For FSD -------------------------------------------
+        elif subject=="Full Stack Development":
+            sql = 'SELECT roll,division FROM fsd WHERE division = "{}"'.format(division)
+            btech.execute(sql)
+            data = btech.fetchall()
+
+            row = []
+            for i in data:
+                row.append(list(i))
+            total['roll'] = row
+
+            new_dates = []
+            for i in dates:
+                try:    
+                    sql = 'SELECT {} FROM fsd WHERE division = "{}"'.format(i,division)
+                    btech.execute(sql)
+                    data = btech.fetchall()
+                    total[i] = data
+                    new_dates.append(i)
+                except:
+                    print('except')
+
+            total['dates'] = new_dates
+
+            for i in range(len(total['dates'])):
+                for j in range(len(total['roll'])):
+                    total['roll'][j].append(total[total['dates'][i]][j][0])
+
+            total['pre'] = [year,division,subject]
+
+        # For SA --------------------------------------------
+        elif subject=="System Administration":
+            sql = 'SELECT roll,division FROM sa WHERE division = "{}"'.format(division)
+            btech.execute(sql)
+            data = btech.fetchall()
+
+            row = []
+            for i in data:
+                row.append(list(i))
+            total['roll'] = row
+
+            new_dates = []
+            for i in dates:
+                try:    
+                    sql = 'SELECT {} FROM sa WHERE division = "{}"'.format(i,division)
+                    btech.execute(sql)
+                    data = btech.fetchall()
+                    total[i] = data
+                    new_dates.append(i)
+                except:
+                    print('except')
+
+            total['dates'] = new_dates
+
+            for i in range(len(total['dates'])):
+                for j in range(len(total['roll'])):
+                    total['roll'][j].append(total[total['dates'][i]][j][0])
+
+            total['pre'] = [year,division,subject]
+
     print(total)
     return total
 
@@ -73,7 +229,7 @@ def classAttendance(year,division,sdate,edate):
     for i in a:
         ss = i.strftime("%Y_%m_%d")
         dates.append(ss)
-    ll = []
+    
     new_dates = []
 
     if year=='BTECH':
@@ -88,6 +244,7 @@ def classAttendance(year,division,sdate,edate):
         total['subs'] = []
         
         # FOR SOFTWARE ENGINEERING---------------
+        ll = []
         total['subs'].append('SE')
         for i in dates:
             try:
@@ -107,6 +264,100 @@ def classAttendance(year,division,sdate,edate):
         for j in range(len(total['roll'])):
             total['roll'][j].append(su[j])
 
-        # FOR DIFFRENT SUBJECT--------------------
+        # FOR Big Data Analysis------------------------------
+        ll = []
+        total['subs'].append('BDA')
+        for i in dates:
+            try:
+                sql = 'SELECT {} AS "total" FROM bda WHERE division = "A"'.format(i)
+                btech.execute(sql)
+                data = btech.fetchall()
+                # print(data)
+                for k in range(len(data)):
+                    data[k] = data[k][0]
+                ll.append(data)
+            except:
+                print('except')
+        su = [sum(x) for x in zip(*ll)]
 
+        for j in range(len(total['roll'])):
+            total['roll'][j].append(su[j])
+
+        # For Cloud Computing ------------------------------
+        ll = []
+        total['subs'].append('CC')
+        for i in dates:
+            try:
+                sql = 'SELECT {} AS "total" FROM cc WHERE division = "A"'.format(i)
+                btech.execute(sql)
+                data = btech.fetchall()
+                # print(data)
+                for k in range(len(data)):
+                    data[k] = data[k][0]
+                ll.append(data)
+            except:
+                print('except')
+        su = [sum(x) for x in zip(*ll)]
+
+        for j in range(len(total['roll'])):
+            total['roll'][j].append(su[j])
+        
+        # For Blockchain Technology ------------------------
+        ll = []
+        total['subs'].append('BT')
+        for i in dates:
+            try:
+                sql = 'SELECT {} AS "total" FROM bt WHERE division = "A"'.format(i)
+                btech.execute(sql)
+                data = btech.fetchall()
+                # print(data)
+                for k in range(len(data)):
+                    data[k] = data[k][0]
+                ll.append(data)
+            except:
+                print('except')
+        su = [sum(x) for x in zip(*ll)]
+
+        for j in range(len(total['roll'])):
+            total['roll'][j].append(su[j])
+
+        # For Full Stacck Development ------------------------
+        ll = []
+        total['subs'].append('FSD')
+        for i in dates:
+            try:
+                sql = 'SELECT {} AS "total" FROM fsd WHERE division = "A"'.format(i)
+                btech.execute(sql)
+                data = btech.fetchall()
+                # print(data)
+                for k in range(len(data)):
+                    data[k] = data[k][0]
+                ll.append(data)
+            except:
+                print('except')
+        su = [sum(x) for x in zip(*ll)]
+
+        for j in range(len(total['roll'])):
+            total['roll'][j].append(su[j])
+
+        # For System Administration --------------------------
+        ll = []
+        total['subs'].append('SA')
+        for i in dates:
+            try:
+                sql = 'SELECT {} AS "total" FROM sa WHERE division = "A"'.format(i)
+                btech.execute(sql)
+                data = btech.fetchall()
+                # print(data)
+                for k in range(len(data)):
+                    data[k] = data[k][0]
+                ll.append(data)
+            except:
+                print('except')
+        su = [sum(x) for x in zip(*ll)]
+
+        for j in range(len(total['roll'])):
+            total['roll'][j].append(su[j])
+
+    print(total)
     return total
