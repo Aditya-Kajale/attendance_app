1b5dc672fa4c40b408fad6ecc87f78fe5e0ecd50
Aditya Kajale <66864217+Aditya-Kajale@users.noreply.github.com>
2023-02-22 17:23:49 +0530

    SY and TY added
    sy and ty code was remaining in previous push
    that code adding in this push

--- studentattendance.py
+++ studentattendance.py
@@ -47,6 +47,70 @@ def studenttAttendance_theory(roll,year,subject):
         for i,j in attendance.items():
             total['col'].append(i)
             total['attendance'].append(j)
+    
+    elif year == 'TY':
+        attendance = {}
+        try:
+            sql = 'SELECT * FROM `{}` WHERE roll = "{}"'.format(subject,roll)
+            ty.execute(sql)
+            data = ty.fetchall()
+            data = list(data[0][3:])
+            sql = "SHOW COLUMNS FROM `{}`".format(subject)
+            ty.execute(sql)
+            col = ty.fetchall()
+            column = []
+            for k in col:
+                column.append(k[0])
+            column = column[3:]
+            # print(data,column)
+        except:
+            print('except')
+        
+        for i in range(len(column)):
+            if data[i] == 1:
+                attendance[column[i]] = 'Present'
+            elif data[i] == 0:
+                attendance[column[i]] = 'Absent'
+            else:
+                pass
+        total['col'] = []
+        total['attendance'] = []
+
+        for i,j in attendance.items():
+            total['col'].append(i)
+            total['attendance'].append(j)
+    
+    elif year == 'SY':
+        attendance = {}
+        try:
+            sql = 'SELECT * FROM `{}` WHERE roll = "{}"'.format(subject,roll)
+            sy.execute(sql)
+            data = sy.fetchall()
+            data = list(data[0][3:])
+            sql = "SHOW COLUMNS FROM `{}`".format(subject)
+            sy.execute(sql)
+            col = sy.fetchall()
+            column = []
+            for k in col:
+                column.append(k[0])
+            column = column[3:]
+            # print(data,column)
+        except:
+            print('except')
+        
+        for i in range(len(column)):
+            if data[i] == 1:
+                attendance[column[i]] = 'Present'
+            elif data[i] == 0:
+                attendance[column[i]] = 'Absent'
+            else:
+                pass
+        total['col'] = []
+        total['attendance'] = []
+
+        for i,j in attendance.items():
+            total['col'].append(i)
+            total['attendance'].append(j)
 
     at_sy.close()
     at_ty.close()
@@ -78,12 +142,78 @@ def studenttAttendance_practical(roll,year,subject):
             for k in col:
                 column.append(k[0])
             column = column[4:]
-            print(data,column)
+            # print(data,column)
         except:
             print('except')
         
         for i in range(len(column)):
-            print(data[i])
+            # print(data[i])
+            if data[i] == 2:
+                attendance[column[i]] = 'Present'
+            elif data[i] == 0:
+                attendance[column[i]] = 'Absent'
+            else:
+                pass
+        total['col'] = []
+        total['attendance'] = []
+
+        for i,j in attendance.items():
+            total['col'].append(i)
+            total['attendance'].append(j)
+
+    elif year == 'TY':
+        attendance = {}
+        try:
+            sql = 'SELECT * FROM `{}` WHERE roll = "{}"'.format(subject,roll)
+            tyP.execute(sql)
+            data = tyP.fetchall()
+            data = list(data[0][4:])
+            sql = "SHOW COLUMNS FROM `{}`".format(subject)
+            tyP.execute(sql)
+            col = tyP.fetchall()
+            column = []
+            for k in col:
+                column.append(k[0])
+            column = column[4:]
+            # print(data,column)
+        except:
+            print('except')
+        
+        for i in range(len(column)):
+            # print(data[i])
+            if data[i] == 2:
+                attendance[column[i]] = 'Present'
+            elif data[i] == 0:
+                attendance[column[i]] = 'Absent'
+            else:
+                pass
+        total['col'] = []
+        total['attendance'] = []
+
+        for i,j in attendance.items():
+            total['col'].append(i)
+            total['attendance'].append(j)
+    
+    elif year == 'SY':
+        attendance = {}
+        try:
+            sql = 'SELECT * FROM `{}` WHERE roll = "{}"'.format(subject,roll)
+            syP.execute(sql)
+            data = syP.fetchall()
+            data = list(data[0][4:])
+            sql = "SHOW COLUMNS FROM `{}`".format(subject)
+            syP.execute(sql)
+            col = syP.fetchall()
+            column = []
+            for k in col:
+                column.append(k[0])
+            column = column[4:]
+            # print(data,column)
+        except:
+            print('except')
+        
+        for i in range(len(column)):
+            # print(data[i])
             if data[i] == 2:
                 attendance[column[i]] = 'Present'
             elif data[i] == 0:
@@ -98,4 +228,7 @@ def studenttAttendance_practical(roll,year,subject):
             total['attendance'].append(j)
 
     print(total)
+    ap_sy.close()
+    ap_ty.close()
+    ap_btech.close()
     return total
